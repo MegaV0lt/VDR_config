409d26d46aaa9b77a132bdee9c60a539d611d8cd
MegaV0lt <Stefan.Blochberger@gmx.de>
2024-02-19 10:41:27 +0100

    f_strstr deaktiviert

--- bin/yavdr_funcs.sh
+++ bin/yavdr_funcs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This file has to be included via source command
-# VERSION=240217
+# VERSION=240219
 
 trap f_exit EXIT
 
@@ -39,7 +39,7 @@ f_log() {  # Gibt die Meldung auf der Konsole und im Syslog aus
   [[ -n "$LOG_FILE" && -w "$LOG_FILE" ]] && echo "$*" >> "$LOG_FILE"  # Log in Datei
 }
 
-f_log2() {                                           
+f_log2() {
   local data=("${@:-$(</dev/stdin)}")               # Akzeptiert Parameter und via stdin (|)
   [[ -t 1 ]] && printf '%s\n' "${data[@]}"          # Konsole falls verbunden
   logger -t "$SELF_NAME" "${data[@]}"               # Systemlog
@@ -47,27 +47,27 @@ f_log2() {
 }
 
 f_rotate_log() {  # Log rotieren wenn zu groß
-  local file="${LOG_FILE:-$1}"
+  local file="${LOG_FILE:-$1}" file_size
   if [[ -n "$file" && -w "$file" ]] ; then  # Datei Existiert und hat Schreibrechte
-    FILE_SIZE="$(stat -c %s "$file" 2>/dev/null)"
-    [[ ${FILE_SIZE:-51201} -gt ${MAX_LOG_SIZE:-51200} ]] && mv --force "$file" "${file}.old"
+    file_size="$(stat -c %s "$file" 2>/dev/null)"
+    [[ ${file_size:-51201} -gt ${MAX_LOG_SIZE:-51200} ]] && mv --force "$file" "${file}.old"
   fi
 }
 
-f_strstr() {  # strstr echoes nothing if s2 does not occur in s1
-  [[ -n "$2" && -z "${1/*$2*}" ]] && return 0  # Gefunden
-  return 1
-}
+#f_strstr() {  # strstr echoes nothing if s2 does not occur in s1
+#  [[ -n "$2" && -z "${1/*$2*}" ]] && return 0  # Gefunden
+#  return 1
+#}
 
 f_svdrpsend() {
   if [[ "${1^^}" == 'MESG' ]] ; then
     mapfile -t < <("$SVDRPSEND" MSGT "${@:2}")  # Prüfen ob VDR den Befehl kennt
     # 220 vdr01 SVDRP VideoDiskRecorder 2.6.1; Thu Oct 27 15:30:24 2022; UTF-8
-    # 501 Missing message / 500 Command unrecognized: "MSGT"
+    # 500 Command unrecognized: "MSGT"
     # 221 vdr01 closing connection
     if [[ "${MAPFILE[1]}" == "500"* ]] ; then  # MSGT nicht vorhanden
-      : "${2#@}" ; clean_arg="${_#%}"          # '%' oder '@' entfernen
-      "$SVDRPSEND" MESG "$clean_arg" "${@:3}"
+      : "${2#@}" ; : "${_#%}"                  # '%' oder '@' entfernen
+      "$SVDRPSEND" MESG "$_" "${@:3}"
     fi
   else
     "$SVDRPSEND" "$@"  # Für alles andere durchreichen
