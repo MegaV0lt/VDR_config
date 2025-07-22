c542313a62eee36b646a904fb4a8f0b7757fdbca
MegaV0lt <Stefan.Blochberger@gmx.de>
2025-07-22 09:43:02 +0200

    Optimiere get_se.sh: Entferne nicht benötigte Funktionen, verbessere die Handhabung von Titel und Kurztext, und passe die Ausgabeformatierung an

--- local/sbin/get_se.sh
+++ local/sbin/get_se.sh
@@ -47,49 +47,11 @@ DATA=("$@")                                       # Übergebene Daten in ein Arr
 TITLE="${DATA[0]}"                                # Titel der Sendung
 SUBTITLE="${DATA[1]}"                             # Kurztext
 #DEBUG='true'                                     # Debug via logger
-
-### Funktionen
-f_shorten_subtitle() {  # Kürzt den Kurztext auf 75 Zeichen
-  local length=75
-  if [[ "${#SUBTITLE}" -ge $length ]] ; then
-      : "${SUBTITLE:0:length}"
-      SUBTITLE="${_%%' '}…"  # Kürzen und Leerzeichen am Ende entfernen
-  fi
-}
+LC_ALL=C                                          # Locale auf C setzen für schnelles Sortieren
 
 ### Start
 
-# Titel von TVScraper verwenden
-if [[ -n "${DATA[9]}" ]] ; then  # Name in externer Datenbank
-  if [[ "${TITLE}" =~ ${DATA[9]} ]] ; then
-    TITLE="${DATA[9]}"             # Titel der Sendung
-  fi
-fi
-
-# Kurztext von TVScraper verwenden  # TODO
-if [[ -n "${SUBTITLE}" && -n "${DATA[10]}" ]] ; then  # Name der Episode
-  # Nur wenn in SUBTITLE enthalten
-  if [[ "${SUBTITLE}" =~ ${DATA[10]} ]] ; then
-    SUBTITLE_TVS="${DATA[10]}"       # Kurztext der Sendung
-  fi
-fi
-
-# Zeichen ersetzen, damit Aufnahmen nicht in unterschiedlichen Ordnern landen
-case "${DATA[0]}" in
-  *' - '*) DATA[0]="${DATA[0]//' - '/' – '}" ;;  # Kurzen durch langen Bindestrich (La_Zona_–_Do_not_cross)
-  *"’"*)   DATA[0]="${DATA[0]//’/\'}"    ;;      # Schräges ’ durch gerades ' (Marvel’s_Runaways)
-  *) ;;
-esac  # Ende Zeichenersetzung
-
-# Titel mit (*) am Ende?
-re='\(.*\)$'
-if [[ "$TITLE" =~ $re ]] ; then     #* Titel enthält Klammern am Ende!
-  FOUND_BRACE="${BASH_REMATCH[0]}"  # Wert speichern '(5/6)'
-  : "${TITLE%"${FOUND_BRACE}"}" ; TITLE="${_%%' '}"  # Klammern (und Leerzeichen) entfernen
-fi
-
-f_shorten_subtitle  # Kurztext kürzen, falls zu lang ist
-
+# Staffel- und Episoden-Nummer ermitteln
 if [[ -z "${DATA[2]}" ]] ; then  # Staffel ist leer. Versuche Informationen aus dem Kurztext zu erhalten
   # EPG Beispiel Canal+ First:
   # Woke~Das Treffen S01 E08. Monate später glaubt Keef, dass er endlich an einem besseren Ort angekommen...
@@ -114,6 +76,7 @@ if [[ -z "${DATA[2]}" ]] ; then  # Staffel ist leer. Versuche Informationen aus 
     done
   fi
 
+  # EPG Beispiel Sky:
   # Wenn in der Beschreibung 'Staffel, Folge' entahlen ist, diese verwenden
   #re='([0-9]+).*Staffel, Folge ([0-9]+)'
   #if [[ -z "$S" && "${DATA[5]:0:25}" =~ $re ]] ; then  #* Beschreibung enthält x. Staffel, Folge x:
@@ -126,32 +89,72 @@ if [[ -z "${DATA[2]}" ]] ; then  # Staffel ist leer. Versuche Informationen aus 
     printf -v S '%02d' "${DATA[7]#0}"  # 01
     printf -v E '%02d' "${DATA[8]#0}"  # 03
   fi
-
-  if [[ -z "$SUBTITLE" ]] ; then  # VDR: Leschs Kosmos~2017-03-07_13|00-Di.
-    printf -v SUBTITLE '%(%Y-%m-%d_%H|%M-%a.)T' "${DATA[6]}"  # Sendezeit, falls Leer
-  fi
-
-  [[ -n "$FOUND_BRACE" ]] && SUBTITLE+=" $FOUND_BRACE"
-
-  # Erstellen von [SxxExx]
-  [[ -n "$S" && -n "$E" ]] && SE="[S${S}E${E}]"
-  [[ "${#SE}" -ge 8 ]] && SUBTITLE+="  $SE"
 fi
+
+# Erstellen von (SxxExx)
+[[ -n "$S" && -n "$E" ]] && SE="(S${S}E${E})"
+
+# Kurztext ist leer?
+if [[ -z "$SUBTITLE" ]] ; then  # VDR: Leschs Kosmos~2017-03-07_13|00-Di.
+  printf -v SUBTITLE '%(%Y-%m-%d_%H|%M-%a.)T' "${DATA[6]}"  # Sendezeit, falls Leer
+fi
+
+# Kurztext kürzen, falls zu lang ist
+if [[ "${#SUBTITLE}" -ge 60 ]] ; then
+    : "${SUBTITLE:0:60}"
+    SUBTITLE="${_%%' '}…"  # Kürzen und Leerzeichen am Ende entfernen
+fi
+
+# Titel mit (*) am Ende?
+re='\(.*\)$'
+if [[ "$TITLE" =~ $re ]] ; then     #* Titel enthält Klammern am Ende!
+  FOUND_BRACE="${BASH_REMATCH[0]}"  # Wert speichern '(5/6)'
+  : "${TITLE%"${FOUND_BRACE}"}" ; TITLE="${_%%' '}"  # Klammern (und Leerzeichen) entfernen
+fi
+
+#Titel von TVScraper verwenden
+if [[ -n "${TITLE}" && -n "${DATA[9]}" ]] ; then  # Name in externer Datenbank
+  # Nur wenn in TITLE enthalten
+  if [[ "${TITLE}" =~ ${DATA[9]} ]] ; then
+    TITLE="${DATA[9]}"       # Titel der Sendung
+  fi
+fi
+
+# Zeichen ersetzen, damit Aufnahmen nicht in unterschiedlichen Ordnern landen
+case "${TITLE}" in
+  *' - '*) TITLE="${TITLE//' - '/' – '}" ;;  # Kurzen durch langen Bindestrich (La_Zona_–_Do_not_cross)
+  *"’"*)   TITLE="${TITLE//’/\'}"    ;;      # Schräges ’ durch gerades ' (Marvel’s_Runaways)
+  *) ;;
+esac  # Ende Zeichenersetzung
+
+# Kurztext von TVScraper verwenden
+if [[ -n "${SUBTITLE}" && -n "${DATA[10]}" ]] ; then  # Name der Episode
+  # Nur wenn in SUBTITLE enthalten
+  if [[ "${SUBTITLE}" =~ ${DATA[10]} ]] ; then
+    SUBTITLE="${DATA[10]}"       # Kurztext der Sendung
+  fi
+fi
+
+# Gefundene Klammern im Titel an Kurztext anhängen (5/6)
+[[ -n "$FOUND_BRACE" ]] && SUBTITLE+=" $FOUND_BRACE"
+
+# SxxExx an Kurztext anhängen
+[[ -n "$SE" && "${#SE}" -ge 8 ]] && SUBTITLE+="  $SE"
 
 # Gefundene Klammern im Titel an Kurztext anhängen
-if [[ -n "$FOUND_BRACE" ]] ; then
-  re='(\(S[0-9]+E[0-9]+\).*)'   # (S01E01)
-  re2='(\[S[0-9]+E[0-9]+\].*)'  # [S01E01]
-  [[ "$SUBTITLE" =~ $re ]] && { SE="${BASH_REMATCH[1]}" ;}
-  [[ "$SUBTITLE" =~ $re2 ]] && { SE2="${BASH_REMATCH[1]}" ;}
-  if [[ -n "$SE" || -n "$SE2" ]] ; then
-    : "${SUBTITLE%  "${SE:-$SE2}"}"
-    SUBTITLE+=" ${FOUND_BRACE}  ${SE2:-$SE2}"
-  fi
-fi
+#if [[ -n "$FOUND_BRACE" ]] ; then
+#  re='(\(S[0-9]+E[0-9]+\).*)'   # (S01E01)
+#  re2='(\[S[0-9]+E[0-9]+\].*)'  # [S01E01]
+#  [[ "$SUBTITLE" =~ $re ]] && { SE="${BASH_REMATCH[1]}" ;}
+#  [[ "$SUBTITLE" =~ $re2 ]] && { SE2="${BASH_REMATCH[1]}" ;}
+#  if [[ -n "$SE" || -n "$SE2" ]] ; then
+#    : "${SUBTITLE%  "${SE:-$SE2}"}"
+#    SUBTITLE+=" ${FOUND_BRACE}  ${SE2:-$SE2}"
+#  fi
+#fi
 
 #! -> Das Skript muss eine Zeichenkette <ohne> Zeilenumbruch zurück geben!
 # echo "=> Antwort: ${TITLE}~${SUBTITLE}" >> "$LOG_FILE"
-echo -n "${TITLE}~${SUBTITLE}"  # Ausgabe an epgSearch
+printf '%s~%s' "${TITLE}" "${SUBTITLE}"  # Ausgabe an epgSearch
 
 exit  # Ende
