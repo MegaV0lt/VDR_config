cdbd47a53e0661d50f53d145d1bdf1ce5bc9cd97
MegaV0lt <M_V0lt@mail.ru>
2025-09-06 08:32:53 +0200

    Update systeminfo.sh

--- local/sbin/systeminfo.sh
+++ local/sbin/systeminfo.sh
@@ -7,15 +7,15 @@ MOUNT_POINT='/mnt/MCP-Server_root'  # Einhängepunkt der Zieldatei
 FILE_PATH='var/www'
 INFO_FILE="${HOSTNAME^^}_Systeminfo.txt"
 
-{ printf '%s %(%d.%m.%Y %R)T %s\n' '<###' -1 '###>'
+{ printf '%s %(%d.%m.%Y %R)T %s\n' "<### Systeminformationen (${HOSTNAME^^}) vom " -1 ' ###>'
   echo '=== inxi -Fxz ==='
   inxi --full --filter --extra  # Systeminformationen
-  echo -e '\n\n=== lspci -v ==='
-  lspci -v
   echo -e '\n\n=== vdr --version ==='
   vdr --version
   echo -e '\n\n=== vdr --showargs ==='
   vdr --showargs
+  echo -e '\n\n=== lspci -v ==='
+  lspci -v
 } > "${MOUNT_POINT}/${FILE_PATH}/${INFO_FILE}" 2> "${MOUNT_POINT}/${FILE_PATH}/${INFO_FILE%.*}.err.txt"
 
 # Ende
