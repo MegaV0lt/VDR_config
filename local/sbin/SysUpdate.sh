d9017a6b7a4a1976dcf461c6367c0c02a1fcb134
MegaV0lt <Stefan.Blochberger@gmx.de>
2025-11-03 15:19:27 +0100

    Update SysUpdate.sh

--- local/sbin/SysUpdate.sh
+++ local/sbin/SysUpdate.sh
@@ -6,39 +6,39 @@
 ### Variablen
 msgERR='\e[1;41m FEHLER! \e[0;1m'  # Anzeige "FEHLER!"
 
-[[ $EUID -ne 0 ]] && SUDO='sudo'  # Kein Root?
+[[ $EUID -ne 0 ]] && { echo -e "$msgERR Das Skript muss als Root ausgeführt werden!\e[0m" >&2 ; exit 1 ;}
 
 echo '--> Hole Updateliste…'
-"$SUDO" apt update
+apt update
 
 echo -e '\n--> Liste der Updates:'
-"$SUDO" apt list --upgradable
+apt list --upgradable
 
 #echo -e -n '\nTaste…' ; read -t 20
 
 echo -e '\n--> Prüfe auf Updates…'
-"$SUDO" apt full-upgrade
+apt full-upgrade
 
 # VDR Skripte
 if [[ -e /_config/.git ]] ; then
   cd /_config || exit 1
   echo -e '\n--> Aktualisiere /_config…'
-  if ! "$SUDO" git pull ; then
+  if ! git stash && ! git pull; then
     echo -e "\n$msgERR /_config konnte nicht aktualisiert werden"
     #echo -e -n '\nTaste drücken oder 10 Sekunden warten…' ; read -t 10
   fi
   echo -e '\n--> Überprüfe/Erzeuge Symlinks und Berechtigungen…'
-  "$SUDO" ./createlinks.sh
+  ./createlinks.sh
 fi
 
 # Wakeupskript
 if [[ -e /usr/share/vdr/shutdown-hooks/S90.acpiwakeup ]] ; then
   echo -e '\n--> Aktualisiere Wakeupskript…'
-  if ! "$SUDO" rm -f /usr/share/vdr/shutdown-hooks/S90.acpiwakeup ; then
+  if ! rm -f /usr/share/vdr/shutdown-hooks/S90.acpiwakeup ; then
     echo -e "\n$msgERR Altes Wakeupskript konnte nicht entfernt werden"
   fi
-  
-  if ! "$SUDO" cp -f /home/darkwing/src/S90.acpiwakeup /usr/share/vdr/shutdown-hooks/S91.acpiwakeup-mv ; then
+
+  if ! cp -f /home/darkwing/src/S90.acpiwakeup /usr/share/vdr/shutdown-hooks/S91.acpiwakeup-mv ; then
     echo -e "\n$msgERR Neues Wakeupskript konnte nicht aktualisiert werden"
   fi
 fi
