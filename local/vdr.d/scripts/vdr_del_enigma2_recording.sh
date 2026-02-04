645c69e593a5e3df0cf2130403ee6243bb7c4ca4
MegaV0lt <Stefan.Blochberger@gmx.de>
2026-02-04 07:30:29 +0100

    Update

--- local/vdr.d/scripts/vdr_del_enigma2_recording.sh
+++ local/vdr.d/scripts/vdr_del_enigma2_recording.sh
@@ -10,6 +10,7 @@
 
 source /_config/bin/yavdr_funcs.sh &>/dev/null
 
+# If f_logger is not defined, define it
 if ! declare -F f_logger &>/dev/null ; then
   f_logger() { logger -t yaVDR "vdr_del_enigma2_recording.sh: $*" ;}
 fi
@@ -19,29 +20,37 @@ if [[ -e "${1}/.linked_from_enigma2" ]] ; then
     ENIGMA_LINK="$(<"${1}/.linked_from_enigma2")"
 fi
 
-TRASHCAN_DIR="${ENIGMA_LINK%/movie/*}/movie/trashcan"
+# Check if trashcan directory exists, create if not ('/media/hdd/movie/trash' is the default in EMC)
+TRASHCAN_DIR="${ENIGMA_LINK%/movie/*}/movie/trash"
 if [[ ! -d "$TRASHCAN_DIR" ]] ; then
-    f_logger "Error: Trashcan directory $TRASHCAN_DIR does not exist. Cannot move Enigma2 recording $ENIGMA_LINK."
-    exit 1
+    mkdir --parents "$TRASHCAN_DIR" || {
+        f_logger "Error: Failed to create trashcan directory $TRASHCAN_DIR"
+        exit 1
+    }
 fi
 
 # Check if the path after /movie conains a slash, indicating a subdirectory
 #if [[ "$ENIGMA_LINK" == *"/movie/"*"/"* ]]; then
-#    # Move the parent directory to trashcan instead
+#    # Move the parent directory to trash instead
 #    if [[ -d "$TRASHCAN_DIR" ]] ; then
 #    mv "${ENIGMA_LINK%/*}" "$TRASHCAN_DIR" || {
 #        f_logger "Error: Failed to move ${ENIGMA_LINK%/*} to $TRASHCAN_DIR"
 #    }
-#    f_logger "Moved Enigma2 recording directory ${ENIGMA_LINK%/*} to trashcan: ${TRASHCAN_DIR}"
+#    f_logger "Moved Enigma2 recording directory ${ENIGMA_LINK%/*} to trash: ${TRASHCAN_DIR}"
 #    else
-#    f_logger "Error: Trashcan directory $TRASHCAN_DIR does not exist. Cannot move Enigma2 recording directory ${ENIGMA_LINK%/*}."
+#    f_logger "Error: Trash directory $TRASHCAN_DIR does not exist. Cannot move Enigma2 recording directory ${ENIGMA_LINK%/*}."
 #    fi
 #    # exit
 #else
-    # Move .ts and associated files to trashcan directory in /media/hdd/movie/trashcan
+    # Move .ts and associated files to trash directory in /media/hdd/movie/trash
     REC_NAME="${ENIGMA_LINK%.ts}"
     for ext in .ts .meta .eit .ts.ap .cuts .sc ; do
-        mv "${REC_NAME}${ext}" "$TRASHCAN_DIR"
+        if [[ -f "${REC_NAME}${ext}" ]] ; then
+            mv "${REC_NAME}${ext}" "$TRASHCAN_DIR" || {
+                f_logger "Error: Failed to move ${REC_NAME}${ext} to $TRASHCAN_DIR"
+                }
+            f_logger "Moved ${REC_NAME}${ext} to trashcan: $TRASHCAN_DIR"
+        fi
     done
 
     # Check for part files (Name_001.ts, Name_002.ts, ...)
